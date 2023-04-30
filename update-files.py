cd82b516680708397fba4115ac7b53a1021cb8a3
Florian Hester <florian@celaeno.org>
2023-04-30 16:56:09 +0200

    Initial stuff for update headers.

--- update-files.py
+++ update-files.py
@@ -22,18 +22,28 @@ def find_all_files():
 def add_header_to_file(cc_file):
     print(f"Updating file '{cc_file}'.")
 
-    f = open(cc_file, "r")
+    f = open(cc_file, "rb")
 
-    contents = f.read()
+    raw_contents = f.read()
 
     f.close()
 
-    if contents.find("namespace v8 {") == -1:
+    contents = str(raw_contents, 'UTF-8')
+
+    if contents.find("fixed-array-inl.h") != -1:
+        print("Header fixed-array-inl.h found.")
+
+        return
+
+    if contents.find("namespace v8 {") != -1:
+        new_value = "#include \"src/objects/fixed-array-inl.h\"\n\nnamespace v8 {"
+        contents = contents.replace("namespace v8 {", new_value, 1)
+    else:
         print("Namespace not found.")
 
-    f = open(cc_file, "w")
+    f = open(cc_file, "wb")
 
-    f.write(contents)
+    f.write(bytes(contents, 'UTF-8'))
 
     f.close()
 
@@ -45,5 +55,6 @@ for cc_file in all_cc_files:
         add_header_to_file(cc_file)
     except Exception as e:
         print(f"Failed to update file '{cc_file}': '{e}'.")
+        raise
 
 
